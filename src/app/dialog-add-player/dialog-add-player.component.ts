ac52feb70ce44ab25e2972ef7605b432956cbf61
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2023-07-29 12:37:48 +0200

    some fixes on add player and a general description

--- src/app/dialog-add-player/dialog-add-player.component.ts
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-add-player',
@@ -9,5 +10,9 @@ export class DialogAddPlayerComponent {
 
   name: string = '';
 
-  onNoClick() {}
+  constructor( private dialogRef: MatDialogRef<DialogAddPlayerComponent>) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
